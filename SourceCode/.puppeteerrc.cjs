d7c63b4652a453920757f09f4b5fc735e6ee84a5
Gurkunwar-Singh <gurkunwar1595.be22@chitkara.edu.in>
2026-04-30 01:17:07 +0530

    Fix: Add Puppeteer config to force Chrome download

--- SourceCode/.puppeteerrc.cjs
+++ SourceCode/.puppeteerrc.cjs
@@ -1,9 +1,15 @@
+// .puppeteerrc.cjs
 const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Set the correct cache path for Render
+  // Specify where browsers should be cached
   cacheDirectory: join('/opt/render/.cache', 'puppeteer'),
+  
+  // Explicitly tell Puppeteer to download Chrome
+  chrome: {
+    skipDownload: false,
+  },
 };
